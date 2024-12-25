e5848f5b1f9c49b7bb1a8e0adc720993d2d6d2c0
Will Bolton <git@bolton.me.uk>
2024-12-25 21:41:16 +0000

    Add abstract class Contest
    Anticipating different kinds of contest we move the boilerplate stuff
    into an abstract class so we can just provide different decideWinner
    functions in new classes.
    
    Also update doc comments to have links to the new class.
    
    Merry Christmas!

--- src/Model.kt
+++ src/Model.kt
@@ -1,19 +1,27 @@
-/** Somebody who wants to be elected in a contest. */
+/** Somebody who wants to be elected in a [contest]. */
 class Candidate(val name: String) {}
 
-/**
- * A contest where a single winner is randomly selected from the given
- * [candidates].
- */
-class SortitionContest(val candidates: Set<Candidate>) {
+/** A race to be elected to a position between some [candidates]. */
+abstract class Contest(val candidates: Set<Candidate>) {
     private var winner: Candidate? = null
+    abstract fun decideWinner(): Candidate
 
     fun getWinner(): Candidate {
-        /* declare a winner at random */
-        val winner = this.winner ?: candidates.random()
+        val winner = this.winner ?: this.decideWinner()
         if (this.winner == null) {
             this.winner = winner
         }
         return winner
+    }
+}
+
+/**
+ * A [contest] where a winner is randomly selected from the given
+ * [candidates].
+ */
+class SortitionContest(candidates: Set<Candidate>) : Contest(candidates) {
+    override fun decideWinner(): Candidate {
+        /* declare a winner at random */
+        return candidates.random()
     }
 }
